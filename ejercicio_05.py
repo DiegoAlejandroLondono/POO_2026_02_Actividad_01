b1e4be63873c8da4250851cb26307babd060b60b
DiegoAlejandroLondono <dilondonoz@unal.edu.co>
2026-09-16 23:14:23 -0500

    Exercise  05

---
+++ ejercicio_05.py
@@ -0,0 +1,37 @@
+
+
+class Operaciones:
+    def __init__(self):
+        self.suma = 0
+        self.x = 0
+        self.y = 0
+
+    def Operacion1(self):
+        self.suma = self.suma + self.x
+
+    def Operacion2(self):
+        self.x = self.x + (self.y)**2
+
+    def Operacion3(self):
+        self.suma = self.suma + (self.x/self.y)
+
+
+class Main:
+    #Se hace dentro del metodo init para que prueba_escritorio sea un atributo de instancia y no de la clase Main.
+    def __init__(self):                                                             
+        prueba_escritorio = Operaciones()
+
+        #Se hizo la versión con input para los atributos como en el ejemlo de la clase
+        prueba_escritorio.suma = float(input("Ingrese el valor de suma: "))                       
+        prueba_escritorio.x = float(input("Ingrese el valor de x: "))
+
+        prueba_escritorio.Operacion1()
+
+        prueba_escritorio.y = float(input("Ingrese el valor de y: "))
+
+        prueba_escritorio.Operacion2()
+        prueba_escritorio.Operacion3()
+
+        print(f"EL VALOR DE LA SUMA ES: {prueba_escritorio.suma}")
+
+Main()
